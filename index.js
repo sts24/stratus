6ecc47b997cb2bbf43dcba1e03a09a6bb9ab51e6
Scott Smith <scottsmith24@gmail.com>
2020-01-05 14:10:25 -0800

    get basic data from fetch api

--- index.js
+++ index.js
@@ -8,11 +8,7 @@ class App extends Component {
 		location: {},
 		weather: {},
 		forecast: [],
-		hasError: false
-	}
-
-	static getDerivedStateFromError(error) {
-		return { hasError: true };
+		hasLoaded: false
 	}
 
 	componentDidMount() {
@@ -28,28 +24,43 @@ class App extends Component {
 
 		getLocation
 			.then(function (value) {
-				axios.get('https://api.weather.gov/points/' + $this.state.location.latitude + ',' + $this.state.location.longitude)
-					.then(res => {
-						$this.setState({ weather: res.data });
-
-						axios.get(res.data.properties.forecast)
-							.then(res => {
-								$this.setState({ forecast: res.data });
-							});
-					});
+				fetch('https://api.weather.gov/points/' + $this.state.location.latitude + ',' + $this.state.location.longitude)
+					.then(res => res.json())
+					.then((data) => {
+						$this.setState({ weather: data })
+						return data
+					})
+					.then(function (data) {
+						fetch(data.properties.forecast)
+							.then(res => res.json())
+							.then((data) => {
+								$this.setState({
+									forecast: data,
+									hasLoaded: true
+								})
+							})
+					})
+					.catch(console.log)
 			});
 
 	}
 
 	render() {
-		return (
-			<ul>
-				{this.state.forecast.properties.periods.map(item => (
-					<li>{JSON.stringify(item)}</li>
-				))}
-			</ul>
-		);
+		if (this.state.hasLoaded) {
+			return (
+				<main>
+					<h1>{this.state.weather.properties.relativeLocation.properties.city}, {this.state.weather.properties.relativeLocation.properties.state}</h1>
 
+					<ul>
+						{this.state.forecast.properties.periods.map(item => (
+							<li key={item.startTime}>{item.temperature}</li>
+						))}
+					</ul>
+				</main>
+			);
+		} else {
+			return null
+		}
 	}
 }
 
